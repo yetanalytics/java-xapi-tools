5767b29d963d15f8de4002832cd7d9478b62bc68
Cliff Casey <cliff@yetanalytics.com>
2024-11-22 10:51:30 -0500

    newlines

--- src/main/java/com/yetanalytics/xapi/model/deserializers/ExtensionDeserializer.java
+++ src/main/java/com/yetanalytics/xapi/model/deserializers/ExtensionDeserializer.java
@@ -35,4 +35,3 @@ public class ExtensionDeserializer extends StdDeserializer<Extensions> {
         }
     }
 }
-
--- src/main/java/com/yetanalytics/xapi/model/deserializers/InteractionTypeDeserializer.java
+++ src/main/java/com/yetanalytics/xapi/model/deserializers/InteractionTypeDeserializer.java
@@ -28,5 +28,3 @@ public class InteractionTypeDeserializer extends StdDeserializer<InteractionType
         }
     }
 }
-
-
