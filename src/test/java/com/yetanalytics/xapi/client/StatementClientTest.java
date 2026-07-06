3b7a1b7b9ae9ec1f29bf5a6fa421f57d910870f2
Cliff Casey <cliff@yetanalytics.com>
2026-07-06 12:56:37 -0400

    fixes an issue where errors caused no consumption of responses, which held conns and blocked the pool

--- src/test/java/com/yetanalytics/xapi/client/StatementClientTest.java
+++ src/test/java/com/yetanalytics/xapi/client/StatementClientTest.java
@@ -25,6 +25,7 @@ import org.testcontainers.containers.GenericContainer;
 import com.fasterxml.jackson.core.exc.StreamReadException;
 import com.fasterxml.jackson.databind.DatabindException;
 import com.yetanalytics.xapi.client.filters.StatementFilters;
+import com.yetanalytics.xapi.exception.StatementClientException;
 import com.yetanalytics.xapi.model.Agent;
 import com.yetanalytics.xapi.model.Statement;
 import com.yetanalytics.xapi.util.Mapper;
@@ -216,5 +217,39 @@ public class StatementClientTest {
         List<Statement> result = client.getStatements(filters);
         assertNotNull(result);
         
+    }
+
+    @Test
+    public void testConnectionPoolOnError() throws StreamReadException, DatabindException, IOException {
+        LRS lrs = new LRS(getMappedHost(), KEY, SECRET);
+        StatementClient client = new StatementClient(lrs);
+        
+        Statement statement = new Statement();
+        statement.setId(UUID.randomUUID());
+
+        // Try 1, handled, no issue
+        try {
+            client.postStatement(statement);
+        } catch (StatementClientException e) {
+            assertTrue(e.getMessage().contains("Error, Non-200 Status. Received: 400"));
+        }
+
+        // Try 2, handled, no issue
+        try {
+            client.postStatement(statement);
+        } catch (StatementClientException e) {
+            assertTrue(e.getMessage().contains("Error, Non-200 Status. Received: 400"));
+        }
+
+        // Try 3, will await indefinitely if connection pool is not cleaned up properly
+        // Test will finish successfully if client is handling conns right
+        try {
+            client.postStatement(statement);
+        } catch (StatementClientException e) {
+            assertTrue(e.getMessage().contains("Error, Non-200 Status. Received: 400"));
+        }
+        
+
+
     }
 }
